4b17a8110bf83f3a41440f1b26faa0dd9b5918e8
MaxBernede <bernede.maximilien@gmail.com>
2023-10-12 02:57:25 +0200

    green square...

--- main.cpp
+++ main.cpp
@@ -1,27 +1,22 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-    // Create a window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    // Create a red circle
-    sf::CircleShape circle(50);
-    circle.setFillColor(sf::Color::Red);
-    circle.setPosition(375, 275);
-
-    while (window.isOpen()) {
-        // Handle events
+    while (window.isOpen())
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
         }
 
         window.clear();
-
-        window.draw(circle);
-
+        window.draw(shape);
         window.display();
     }
 
